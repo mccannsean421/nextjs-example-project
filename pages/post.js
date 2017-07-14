85980fc489198025f56cae8a021795767cb4bd23
mccannsean421 <mccannsean421@gmail.com>
2017-07-14 07:44:44 +0100

    Passing url via props

--- pages/post.js
+++ pages/post.js
@@ -1,8 +1,15 @@
 import Layout from '../comps/MyLayout.js'
 
+const Content = (props) => (
+  <div>
+    <h1>{props.url.query.title}</h1>
+    <p>This is the blog post content.</p>
+  </div>
+)
+
+//passing URL via props as URL is only available to the pages main component
 export default (props) => (
-	<Layout>
-		<h1>{props.url.query.title}</h1>
-		<p>this is a blog post</p>
-	</Layout>
+    <Layout>
+       <Content url={props.url} />
+    </Layout>
 )
